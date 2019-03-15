491e156f980bfd833c67c6ea4dc063676effa44e
haifeng zhang <haifeng.zhang@ninebot.com>
2019-03-15 15:00:25 +0800

    setup

---
+++ include/generated/timestamp_autogenerated.h
@@ -0,0 +1,4 @@
+#define U_BOOT_DATE "Mar 14 2019"
+#define U_BOOT_TIME "15:09:00"
+#define U_BOOT_TZ "+0800"
+#define U_BOOT_DMI_DATE "03/14/2019"
---
+++ include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2017.01-rc2-00057-g32ab180-dirty"
+#define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
+#define CC_VERSION_STRING "arm-linux-gnueabihf-gcc (Ubuntu/Linaro 5.4.0-6ubuntu1~16.04.9) 5.4.0 20160609"
+#define LD_VERSION_STRING "GNU ld (GNU Binutils for Ubuntu) 2.26.1"
